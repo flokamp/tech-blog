69c642ef3e3fdadce9eebf700e1bd2942329b7b3
flokamp <74511633+flokamp@users.noreply.github.com>
2021-03-24 20:06:39 -0500

    server.js setup

---
+++ server.js
@@ -0,0 +1,46 @@
+// Dependencies
+// =============================================================
+const express = require("express");
+const exphbs = require("express-handlebars");
+const routes = require("./controllers");
+const sequelize = require("./config/connection");
+const path = require("path");
+const session = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+// Sets up the session
+// =============================================================
+const sess = {
+	secret: "Super secret secret",
+	cookie: {},
+	resave: false,
+	saveUninitialized: true,
+	store: new SequelizeStore({
+		db: sequelize,
+	}),
+};
+
+// Sets up the Express App
+// =============================================================
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Set Handlebars as the default template engine
+// =============================================================
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.use(session(sess));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+// turn on routes
+// =============================================================
+app.use(routes);
+
+// turn on connection to db and server
+// =============================================================
+sequelize.sync({ force: false }).then(() => {
+	app.listen(PORT, () => console.log("Now listening"));
+});
