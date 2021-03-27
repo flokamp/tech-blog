052eb15b6c009a62e9e6465d76236b44a6a1170d
flokamp <74511633+flokamp@users.noreply.github.com>
2021-03-27 18:38:12 -0500

    added remaining models and some css styling

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
-// Dependencies
-// =============================================================
-const express = require("express");
-const controllers = require("./controllers");
-const exphbs = require("express-handlebars");
-const sequelize = require("./config/connection");
 const path = require("path");
+const express = require("express");
 const session = require("express-session");
+const exphbs = require("express-handlebars");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sequelize = require("./config/connection");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
-// Set up the session
-// =============================================================
 const sess = {
 	secret: "Super secret secret",
 	cookie: {},
@@ -20,26 +19,21 @@ const sess = {
 	}),
 };
 
-// Sets up the Express App
-// =============================================================
-const app = express();
-const PORT = process.env.PORT || 3001;
+app.use(session(sess));
 
-// Set Handlebars as the default template engine
-// =============================================================
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+const helpers = require("./utils/helpers");
+
+const hbs = exphbs.create({ helpers });
+
+app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
-// Set up middlewares
-// =============================================================
-app.use(session(sess));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
-app.use(controllers);
 
-// Turn on connection to db and server
-// =============================================================
+app.use(require("./controllers/"));
+
 sequelize.sync({ force: false }).then(() => {
 	app.listen(PORT, () => console.log("Now listening"));
 });
