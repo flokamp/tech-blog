5c2fc3a225cad37956f1380943d7bddc780afc2d
flokamp <74511633+flokamp@users.noreply.github.com>
2021-03-30 00:58:25 -0500

    finally figured out tabs

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,6 +1,6 @@
 function openTab(tabName) {
 	var i;
-	var x = document.getElementsByClassName("tab");
+	var x = document.getElementsByClassName("form");
 	for (i = 0; i < x.length; i++) {
 		x[i].style.display = "none";
 	}
